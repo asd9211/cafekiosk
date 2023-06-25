9584534e426800fc3fe1f70456a66a9ff5331536
asd9211 <drrg@naver.com>
2023-06-25 17:39:42 +0900

    테스트 케이스 세분화하기

---
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,47 @@
+package sample.cafekiosk.unit;
+
+import lombok.Getter;
+import sample.cafekiosk.unit.beverages.Beverage;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void add(Beverage beverage, int count) {
+        if(count <= 0){
+            throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
+        }
+
+        for(int i = 0 ; i < count; i++){
+            beverages.add(beverage);
+        }
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
+    }
+
+    public int calculateTotalPrice() {
+        return beverages.stream()
+                .mapToInt(b -> b.getPrice())
+                .sum();
+    }
+
+    public Order createOrder(){
+        return new Order(LocalDateTime.now(), beverages);
+    }
+}
